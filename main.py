4182235bbb214518e4611e3d2ee9a3eb81f087aa
churakaagii <a.r.nakama@gmail.com>
2017-01-24 13:20:29 -0500

    build with all features implemented

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 import webapp2, caesar
+from cgi import escape
 
-form = """
-    <form>
-        <textarea>hello butts</textarea>
+def build_page(areamessage="", setnum=0):
+    form = """
+    <h1>A simple Caesar cypher</h1>
+    <form method='post'>
+        <textarea name='message' style='height: 100px; width: 400px;'>%s</textarea>
+        <br/>
+        <label>
+            Rotate by:
+            <input type='number' name='rotnum' value='%s'>
+        </label>
         <br/>
         <input type='submit'/>
     </form>
+    <p><strong>A tip!</strong> ROT-13 twice for double the security!</p>
+    <p style='font-size: 0.8em;'>Coded by Ada Nakama</p>
     """
+    return form % (areamessage, setnum)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write(form)
+        page = build_page("Please type the text to be encrypted here", 13)
+        self.response.write(page)
+        
+    def post(self):
+        rotnum = self.request.get("rotnum")
+        if rotnum == "":
+            rotnum = 0
+            encmessage = "Error: please specify a valid rotation number"
+        else:
+            rotnum = int(rotnum)
+            encmessage = caesar.encrypt(self.request.get("message"), rotnum)
+        page = build_page(escape(encmessage), rotnum)
+        self.response.write(page)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
